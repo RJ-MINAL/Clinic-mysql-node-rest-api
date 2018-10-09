ba23832bbd2709fb4168a7f1644a88fb396d10c6
RJ_Minal <rj.minera@gmail.com>
2018-10-09 12:01:42 -0600

    fix: body request for POST & cleaner code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
+const http = require('http');
 const app = require('./config/server');
 const avatar = require('./routes/avatar.route');
 
+// routes
 app.use('/api/avatar', avatar);
 
 app.get('/', (req, res) => {
   res.redirect('/api/avatar/');
 });
 
-app.listen(app.get('port'), () => {
-  console.log('server on port ', app.get('port'));
+// server up
+http.createServer(app).listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'));
 });
+
+// app.listen(app.get('port'), () => {
+//   console.log('server on port ', app.get('port'));
+// });
--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -7,10 +8,14 @@ const app = express();
 // settings
 app.set('port', process.env.PORT || 4200);
 //app.set('view engine', 'ejs');
-//app.set('views', path.join(__dirname, '../app/views'));
+//app.set('views', path.join(__dirname, '../src/views'));
 
 // middlewares
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../static')));
+app.use(bodyParser.json());
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 module.exports = app;
